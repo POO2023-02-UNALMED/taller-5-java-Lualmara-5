53af7e4246c5cda89d1deb3e5ae30c3efc0b0d5b
Lualmara-5 <luimartinezra@unal.edu.co>
2023-09-24 18:12:59 -0500

    Pez y Reptil

--- src/test/java/zooAnimales/Pez.java
+++ src/test/java/zooAnimales/Pez.java
@@ -8,3 +8,53 @@ public class Pez extends Animal {
 	public static int bacalaos;
 	private String colorEscamas;
 	private int cantidadAletas;
+	
+	
+	public Pez() {
+		this(null,0,null,null,null,0);
+	}
+	public Pez(String nombre, int edad, String habitat, String genero,String colorEscamas,int cantidadAletas) {
+		super(nombre, edad, habitat, genero);
+		this.colorEscamas=colorEscamas;
+		this.cantidadAletas=cantidadAletas;
+		listado.add(this);
+	}
+	
+	public ArrayList<Pez> getListado() {
+		return listado;
+	}
+	
+	public void setListado(ArrayList<Pez> listado) {
+		Pez.listado = listado;
+	}
+	
+	public String getColorEscamas() {
+		return colorEscamas;
+	}
+	
+	public void setColorEscamas(String colorEscamas) {
+		this.colorEscamas = colorEscamas;
+	}
+	
+	public int getCantidadAletas() {
+		return cantidadAletas;
+	}
+	
+	public void setCantidadAletas(int cantidadAletas) {
+		this.cantidadAletas = cantidadAletas;
+	}
+	
+	public static int cantidadPeces() {
+		return listado.size();
+	}
+	
+	public static Animal crearSalmon(String nombre, int edad, String genero) {
+		salmones++;
+		return new Pez(nombre,edad,"oceano", genero,"rojo",6);
+	}
+	
+	public static Animal crearBacalao(String nombre, int edad, String genero) {
+		bacalaos++;
+		return new Pez(nombre,edad,"oceano", genero,"gris",6);
+	}
+}
--- src/test/java/zooAnimales/Reptil.java
+++ src/test/java/zooAnimales/Reptil.java
@@ -8,3 +8,53 @@ public class Reptil extends Animal{
 	public static int serpientes;
 	private String colorEscamas;
 	private int largoCola;
+	
+	public Reptil() {
+		this(null,0,null,null,null,0);
+	}
+	
+	public Reptil(String nombre, int edad, String habitat, String genero,String colorEscamas, int largoCola) {
+		super(nombre,edad,habitat,genero);
+		this.colorEscamas=colorEscamas;
+		this.largoCola=largoCola;
+		listado.add(this);
+	}
+	
+	public ArrayList<Reptil> getListado() {
+		return listado;
+	}
+	
+	public void setListado(ArrayList<Reptil> listado) {
+		Reptil.listado = listado;
+	}
+	
+	public String getColorEscamas() {
+		return colorEscamas;
+	}
+	
+	public void setColorEscamas(String colorEscamas) {
+		this.colorEscamas = colorEscamas;
+	}
+	
+	public int getLargoCola() {
+		return largoCola;
+	}
+	
+	public void setLargoCola(int largoCola) {
+		this.largoCola = largoCola;
+	}
+	
+	public static int cantidadReptiles() {
+		return listado.size();
+	}
+	
+	public static Animal crearSerpiente(String nombre, int edad, String genero) {
+		serpientes++;
+		return new Reptil(nombre,edad,"jungla",genero,"blanco",1);
+	}
+	
+	public static Animal crearIguana(String nombre, int edad, String genero) {
+		iguanas++;
+		return new Reptil(nombre,edad,"humedal",genero,"verde",3);
+	}
+}
