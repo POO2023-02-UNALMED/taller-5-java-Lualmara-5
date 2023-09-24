cffab40dc17559c99a52e828d6f4cf885693080f
Lualmara-5 <luimartinezra@unal.edu.co>
2023-09-24 16:14:52 -0500

    Class Mamifero Part 1

--- src/test/java/zooAnimales/Mamifero.java
+++ src/test/java/zooAnimales/Mamifero.java
@@ -1,5 +1,10 @@
 package zooAnimales;
 
-public class Mamifero {
+import java.util.ArrayList;
 
-}
+public class Mamifero extends Animal{
+	private static ArrayList<Mamifero> listado=new ArrayList <>();;
+	public static int caballos;
+	public static int leones;
+	private boolean pelaje;
+	private int patas;
