6ba879f2c8af114d018f8b9833b1c178b37b6134
Lualmara-5 <luimartinezra@unal.edu.co>
2023-09-24 16:54:03 -0500

    Update Anfibio.java

--- src/test/java/zooAnimales/Anfibio.java
+++ src/test/java/zooAnimales/Anfibio.java
@@ -12,6 +12,7 @@ public class Anfibio extends Animal {
 	public Anfibio() {
 		this(null,0,null,null,null,false);
 	}
+	
 	public Anfibio(String nombre, int edad, String habitat, String genero,String colorPiel,boolean venenoso) {
 		super(nombre, edad, habitat, genero);
 		this.colorPiel=colorPiel;
@@ -22,7 +23,38 @@ public class Anfibio extends Animal {
 	public ArrayList<Anfibio> getListado() {
 		return listado;
 	}
+	
 	public void setListado(ArrayList<Anfibio> listado) {
 		Anfibio.listado = listado;
 	}
-]
+	
+	public String getColorPiel() {
+		return colorPiel;
+	}
+	
+	public void setColorPiel(String colorPiel) {
+		this.colorPiel = colorPiel;
+	}
+	
+	public boolean isVenenoso() {
+		return venenoso;
+	}
+	
+	public void setVenenoso(boolean venenoso) {
+		this.venenoso = venenoso;
+	}
+	
+	public static int cantidadAnfibios() {
+		return listado.size();
+	}
+	
+	public static Animal crearRana(String nombre, int edad, String genero) {
+		ranas++;
+		return new Anfibio(nombre, edad,"selva",genero,"rojo",true);
+	}
+	
+	public static Animal crearSalamandra(String nombre, int edad, String genero) {
+		salamandras++;
+		return new Anfibio(nombre, edad,"selva",genero,"negro y amarillo",false);
+	}
+}
