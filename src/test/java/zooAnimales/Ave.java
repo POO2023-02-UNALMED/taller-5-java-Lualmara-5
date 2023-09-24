9868da377545fe9054cbbd31da6ebb6ab32b484c
Lualmara-5 <luimartinezra@unal.edu.co>
2023-09-24 17:32:20 -0500

    Update Ave.java

--- src/test/java/zooAnimales/Ave.java
+++ src/test/java/zooAnimales/Ave.java
@@ -7,3 +7,45 @@ public class Ave extends Animal{
 	public static int halcones; 
 	public static int aguilas;
 	private String colorPlumas;
+	
+	public Ave() {
+		this(null,0,null,null,null);
+	}
+	
+	public Ave(String nombre, int edad, String habitat, String genero, String colorPlumas) {
+		super(nombre,edad,habitat,genero);
+		this.colorPlumas=colorPlumas;
+		listado.add(this);
+	}
+	
+	public ArrayList<Ave> getListado() {
+		return listado;
+	}
+	
+	public void setListado(ArrayList<Ave> listado) {
+		Ave.listado = listado;
+	}
+	
+	public String getColorPlumas() {
+		return colorPlumas;
+	}
+	
+	public void setColorPlumas(String colorPlumas) {
+		this.colorPlumas = colorPlumas;
+	}
+	
+	public static int cantidadAves() {
+		return listado.size();
+	}
+	
+	public static Animal crearHalcon(String nombre, int edad, String genero) {
+		halcones++;
+		return new Ave(nombre,edad,"montanas",genero,"cafe glorioso");
+	}
+	
+	public static Animal crearAguila(String nombre, int edad, String genero) {
+		aguilas++;
+		return new Ave(nombre,edad,"montanas",genero,"blanco y amarillo");
+	}
+	
+}
