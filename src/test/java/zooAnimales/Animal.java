0261d53ec043839af651bfca9e98bbca2e021371
Lualmara-5 <luimartinezra@unal.edu.co>
2023-09-24 16:07:25 -0500

    Class Animal Part 1

--- src/test/java/zooAnimales/Animal.java
+++ src/test/java/zooAnimales/Animal.java
@@ -1,5 +1,18 @@
 package zooAnimales;
 
+import java.util.ArrayList;
+
+import gestion.*;
+
 public class Animal {
 
-}
+	private int totalAnimales;
+	private String nombre;
+	private int edad;
+	private String habitat;
+	private String genero;
+	private ArrayList<Zona> zona;
+	
+	public Animal() {
+		this(null,0,null,null);
+	}
