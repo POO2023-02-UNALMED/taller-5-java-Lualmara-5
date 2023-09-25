06bc74cb1965f652f1d9522963f90fd7dc270fd4
Lualmara-5 <luimartinezra@unal.edu.co>
2023-09-24 20:03:43 -0500

    Update Animal.java

--- src/test/java/zooAnimales/Animal.java
+++ src/test/java/zooAnimales/Animal.java
@@ -24,4 +24,83 @@ public class Animal {
 		this.genero = genero;
 		totalAnimales++;
 	}
+	
+	public int getTotalAnimales() {
+		return totalAnimales;
+	}
+
+	public void setTotalAnimales(int totalAnimales) {
+		this.totalAnimales = totalAnimales;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getHabitat() {
+		return habitat;
+	}
+
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public ArrayList<Zona> getZona() {
+		return zona;
+	}
+
+	public void setZona(ArrayList<Zona> zona) {
+		this.zona = zona;
+	}
+	
+	public String movimiento() {
+		if(this instanceof Mamifero) {
+			return "desplazarse";
+		}
+		else if(this instanceof Ave) {
+			return "volar";
+		}
+		else if(this instanceof Reptil) {
+			return "reptar";
+		}
+		else if(this instanceof Pez) {
+			return "nadar";
+		}
+		else {
+			return "saltar";
+		}
+	}
+	
+	public static String totalPorTipo() {
+		return "Mamiferos: " + Mamifero.cantidadMamiferos()+"\n"+"Aves: " + Ave.cantidadAves()+"\n"+"Reptiles: " + Reptil.cantidadReptiles()+"\n"+"Peces: " + Pez.cantidadPeces()+"\n"+"Anfibios: "+ Anfibio.cantidadAnfibios();
+	}
+		
+	public String toString() {
+		if(zona!=null && zona.get(0).getZoo()!=null) {
+			return "Mi nombre es "+ this.nombre+", tengo una edad de "+ this.edad +", habito en "+ this.habitat+ " y mi genero es "+ this.genero+",la zona en la que me ubico es "+ zona.get(0)+", en el "+ zona.get(0).getZoo();
+		}
+		else {
+			return "Mi nombre es "+ this.nombre+", tengo una edad de "+ this.edad +", habito en "+ this.habitat+ " y mi genero es "+ this.genero;
+		}
+	}
 }
